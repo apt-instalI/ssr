4eb03e734d852a9cee7c61958c3f7487240e2d1e
Nice <47160032+apt-instalI@users.noreply.github.com>
2022-07-29 12:44:16 +0800

    开卡监控

--- jd_opencardCheck.py
+++ jd_opencardCheck.py
@@ -2,7 +2,6 @@
 # -*- coding: utf8 -*-
 """
 # 注意！不支持青龙2.10.3以下版本
-# 此脚本需要安装第三方依赖：deepdiff
 # 如果请求api.github.com失败请自行想办法，用拉库用的Github加速代理是没用的
 # 青龙里面要有监控的Github仓库的拉库命令，通过监控Github仓库来查看是否有新的开卡脚本
 # 如果发现新的开卡脚本则自动拉库并运行开卡脚本，如果发现已有多个相同开卡脚本时
@@ -11,26 +10,27 @@
 # 填写要监控的GitHub仓库的 用户名/仓库名/分支/脚本关键词
 # 监控多个仓库请用 & 隔开
 export GitRepoHost="KingRan/KR/main/opencardL&feverrun/my_scripts/main/jd_opencard&smiek2121/scripts/master/opencard&okyyds/yyds/master/lzdz1"
-# http代理，访问不了github的可以填上，无账号密码的直接填“http://ip:端口”
-export GitProxy="http://账号:密码@127.0.0.1:8080"
+# GitHub Token，可以将访问次数提升到5000次，默认60次
+export GitToken="用户名&Token"
+# http代理，访问不了github的可以填上
+export GitProxy="http://127.0.0.1:8080"
 # 运行开卡脚本前禁用开卡脚本定时任务，不填则不禁用，保留原有定时
 export opencardDisable="true"
 
+version: 7.29.1
 cron: */5 0-3 * * *
 new Env('开卡更新检测')
 """
 
 from time import sleep
 from notify import send
-import requests,deepdiff,json,os
+import requests,json,os,difflib
 
-# 显示日志
 List=[]
 def log(content):
     print(content)
     List.append(content)
 
-# 读取青龙登录token
 def qltoken():
     if os.path.exists("/ql/data"):
         path = "/ql/data"
@@ -45,7 +45,6 @@ def qltoken():
         log("青龙Token获取失败")
         return
 
-# 检查版本
 def qlversion():
     url = qlhost+"/system"
     rsp = session.get(url=url,headers=headers)
@@ -63,9 +62,6 @@ def qlversion():
                     version = {"path":"/ql","api":"/crons","id":"id"}
                 elif int(v[0])<=2 and int(v[1])>=10 and int(v[2])>=3: # 大于等于2.10.3 小于2.11.0
                     version = {"path":"/ql","api":"/crons","id":"_id"}
-                else:
-                    log("青龙版本低于2.10.3，停止运行")
-                    return False
             else:
                 version = {"path":"/ql","api":"/crons","id":"_id"}
             return version
@@ -75,7 +71,6 @@ def qlversion():
             log(f'错误信息：{jsons["message"]}')
         return False
 
-# 获取id
 def qlcron(name,repopath):
     url = qlhost+"/crons?searchValue="+name
     if version["api"]=="/subscriptions" and repopath!="False":
@@ -102,9 +97,7 @@ def qlcron(name,repopath):
             log(f'错误信息：{jsons["message"]}')
         return False,False
 
-# 运行拉库命令
 def qlrepo(scriptsName):
-    # 检查仓库文件夹格式
     repopath = Repo[0]+"_"+Repo[1]
     if os.path.exists(path+"/scripts/"+repopath):
         if os.path.exists(path+"/scripts/"+repopath+"_"+Repo[2]):
@@ -120,32 +113,31 @@ def qlrepo(scriptsName):
     if not RepoName:
         log(f"获取仓库任务信息失败：{GitRepo}")
         return
-    # 检查仓库文件夹中是否有开卡脚本，没有就运行拉库命令
-    scriptsFile = os.path.exists(path+"/scripts/"+repopath+"/"+scriptsName)
-    while not scriptsFile:
-        log("没有找到脚本："+repopath+"/"+scriptsName)
-        rsp = session.put(url=url,headers=headers,data=json.dumps(RepoID))
-        if rsp.status_code == 200:
-            log(f"运行拉库任务：{RepoName}")
+    rsp = session.put(url=url,headers=headers,data=json.dumps(RepoID))
+    if rsp.status_code == 200:
+        log(f"运行拉库任务：{RepoName}")
+        ii=0
+        scriptsFile = path+"/scripts/"+repopath+"/"+scriptsName
+        while not os.path.exists(scriptsFile):
+            if ii>=60:
+                log(f"找不到文件：{scriptsFile}")
+                return
+            sleep(1)
+            ii+=1
         else:
-            log(f'请求青龙失败：{url}')
-            if "message" in rsp.json():
-                log(f'错误信息：{rsp.json()["message"]}')
-            return
-        sleep(10)
-        scriptsFile = os.path.exists(path+"/scripts/"+repopath+"/"+scriptsName)
+            sleep(5)
+            return True
     else:
-        log("找到开卡脚本："+repopath+"/"+scriptsName)
-        return True
+        log(f'请求青龙失败：{url}')
+        if "message" in rsp.json():
+            log(f'错误信息：{rsp.json()["message"]}')
+        return
 
-# 运行开卡任务
 def qltask(scriptsName):
-    # 获取开卡任务信息
     TaskName,TaskID = qlcron(scriptsName,"False")
     if not TaskName:
         log(f"获取开卡任务信息失败：{scriptsName}")
         return
-    # 禁用开卡任务
     if 'opencardDisable' in os.environ:
         Disable = os.environ['opencardDisable']
         if Disable=="true":
@@ -158,98 +150,93 @@ def qltask(scriptsName):
                 if "message" in rsp.json():
                     log(f'错误信息：{rsp.json()["message"]}')
                 return
-    # 查找当前是否有多个同名开卡任务
-    namename = TaskName.split(" ")
-    if len(namename)>1:
-        namename = namename[1]
-    else:
-        namename = namename[0]
-    url = qlhost+"/crons?searchValue="+namename
-    rsp = session.get(url=url, headers=headers).json()
-    if len(rsp["data"])>1:
-        log(f"找到多个任务：{namename}")
-        # 查看任务是否运行过
-        for a in rsp["data"]:
-            xurl = qlhost+"/crons/"+str(a[version["id"]])
-            xrsp = session.get(url=xurl, headers=headers).json()
-            if "last_execution_time" in xrsp["data"]:
-                if xrsp["data"]["last_execution_time"]!=0:
-                    log("该任务曾运行："+xrsp["data"]["name"])
+    if not os.path.exists(f"./nameCron.json"):
+        with open(f"./nameCron.json","w") as f:
+            json.dump({},f)
+        log(f"没有找到nameCron.json文件！将自动生成")
+    with open('./nameCron.json',"r",encoding='UTF-8') as f:
+        TaskStr = json.load(f)
+    for i in TaskStr:
+        if i != Repo[0]:
+            for x in TaskStr[i]:
+                point = round(difflib.SequenceMatcher(None,TaskName,x).quick_ratio()*100)
+                if point>=70:
+                    log(f"任务名高度相似：{TaskName}/{x}={point}%")
                     log("放弃运行任务："+TaskName)
+                    with open(f"./nameCron.json","w",encoding='UTF-8') as f:
+                        json.dump(TaskStr,f)
+                        log(f"保存任务名到nameCron.json文件")
                     return
-                else:
-                    log("从未运行任务："+xrsp["data"]["name"])
-            else:
-                log("从未运行任务："+xrsp["data"]["name"])
     # 运行开卡任务
     url = qlhost+"/crons/run"
     rsp = session.put(url=url,headers=headers,data=json.dumps(TaskID))
     if rsp.status_code == 200:
         log(f"运行开卡任务：{TaskName}")
+        with open(f"./nameCron.json","w",encoding='UTF-8') as f:
+            if Repo[0] not in TaskStr:
+                # log("未找到key："+Repo[0])
+                TaskStr[Repo[0]]=[]
+            TaskStr[Repo[0]].append(TaskName)
+            json.dump(TaskStr,f)
+            log(f"保存任务名到nameCron.json文件")
     else:
         log(f'请求青龙失败：{url}')
         if "message" in rsp.json():
             log(f'错误信息：{rsp.json()["message"]}')
 
-# 请求Github仓库获取目录树
 def github():
-    proxies = {}
-    if 'GitProxy' in os.environ:
-        log("已设置HTTP代理，将通过代理访问api.github.com")
-        proxies['https'] = os.environ['GitProxy']
-    log(f"监控仓库：https://github.com/{GitRepo}")
+    log(f"\n监控仓库：https://github.com/{GitRepo}")
     gitapi = f'https://api.github.com/repos/{GitRepo}/git/trees/{GitBranch}'
-    rsp = session.get(url=gitapi,headers={"Content-Type":"application/json"},proxies=proxies)
+    if 'GitToken' in os.environ:
+        GitToken = os.environ['GitToken'].split("&")
+        rsp = session.get(url=gitapi,auth=(GitToken[0],GitToken[1]),headers={"Content-Type":"application/json"},proxies=proxies)
+    else:
+        rsp = session.get(url=gitapi,headers={"Content-Type":"application/json"},proxies=proxies)
     if rsp.status_code != 200:
         log(f'请求GitHub失败：{gitapi}')
         if "message" in rsp.json():
             log(f'错误信息：{rsp.json()["message"]}')
-    # 只保存目录树中的开卡脚本的文件名信息
-    tree = []
-    for x in rsp.json()["tree"]:
-        if Repo[3] in x["path"]:
-            tree.append(x["path"])
-    return tree
+        return False
+    else:
+        tree = []
+        for x in rsp.json()["tree"]:
+            if Repo[3] in x["path"]:
+                tree.append(x["path"])
+        return tree
 
 def check():
     state = False
-    # 查看是否有tree_库名.json文件，没有则自动生成
-    if not os.path.exists(f"{path}/scripts/tree_{Repo[0]}.json"):
-        with open(f"{path}/scripts/tree_{Repo[0]}.json","w") as f:
-            json.dump(tree,f)
-        log(f"没有找到tree_{Repo[0]}.json文件！将自动生成")
-    # 读取上一次保存的tree.json并与当前tree进行对比
-    with open(f"{path}/scripts/tree_{Repo[0]}.json", 'rb') as json_file:
-        tree_json = json.load(json_file)
-    diff = deepdiff.DeepDiff(tree_json, tree)
-    # 判断是否有新增开卡脚本
-    if "values_changed" in diff:
-        for x in diff["values_changed"]:
-            scriptsName = diff["values_changed"][x]["new_value"]
+    if not os.path.exists(f"./nameScripts.json"):
+        with open(f"./nameScripts.json","w") as f:
+            json.dump({},f)
+        log(f"没有找到nameScripts.json文件！将自动生成")
+    with open(f"./nameScripts.json", 'rb') as json_file:
+        scriptsJson = json.load(json_file)
+        if Repo[0] not in scriptsJson:
+            scriptsJson[Repo[0]]=tree
+            # log("nameScripts.json中未找到KEY："+Repo[0])
+    for scriptsName in tree:
+        if scriptsName not in scriptsJson[Repo[0]]:
             log(f"新增开卡脚本：{scriptsName}")
-            repoPull = qlrepo(scriptsName) # 运行拉库任务
-            if repoPull: # 运行脚本任务
-                qltask(scriptsName)
-            state = True
-            break
-    elif "iterable_item_added" in diff:
-        for x in diff["iterable_item_added"]:
-            scriptsName = diff["iterable_item_added"][x]
-            log(f"新增开卡脚本：{scriptsName}")
-            repoPull = qlrepo(scriptsName) # 运行拉库任务
-            if repoPull: # 运行脚本任务
+            repoPull = qlrepo(scriptsName)
+            if repoPull:
                 qltask(scriptsName)
             state = True
             break
     else:
         log("没有新增开卡脚本")
-    with open(f"{path}/scripts/tree_{Repo[0]}.json","w") as f:
-        log(f"保存数据到tree_{Repo[0]}.json文件")
-        json.dump(tree,f)
+    with open(f"./nameScripts.json","w") as f:
+        scriptsJson[Repo[0]]=tree
+        json.dump(scriptsJson,f)
+        log(f"保存文件名到nameScripts.json文件")
     return state
 
 if 'GitRepoHost' in os.environ:
     RepoHost = os.environ['GitRepoHost'].split("&")
+    proxies = {}
+    if 'GitProxy' in os.environ:
+        log("已设置HTTP代理，将通过代理访问api.github.com")
+        proxies['https'] = os.environ['GitProxy']
     session = requests.session()
     qlhost = 'http://127.0.0.1:5700/api'
     token = qltoken()
@@ -261,10 +248,11 @@ if 'GitRepoHost' in os.environ:
             Repo = RepoX.split("/")
             GitRepo = Repo[0]+"/"+Repo[1]
             GitBranch = Repo[2]
-            tree = github() # 获取github仓库目录树
-            state = check() # 检测有无开卡脚本更新并运行
-            tt = '\n'.join(List)
-            if state:
-                send('开卡更新检测', tt)
+            tree = github()
+            if tree:
+                state = check()
+                tt = '\n'.join(List)
+                if state:
+                    send('开卡更新检测', tt)
 else:
     log("请查看脚本注释后设置相关变量")
